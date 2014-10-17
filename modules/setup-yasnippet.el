10853e28acb0635eaf3c8ea43819eaa0d6c32ae8
Ryo Fukumuro <rkworks@o0o.co>
2014-10-17 18:26:37 +0900

    wip

--- modules/setup-yasnippet.el
+++ modules/setup-yasnippet.el
@@ -13,14 +13,40 @@
           (yas-global-mode 1)
 
           (eval-after-load "helm"
-            '(use-package helm-c-yasnippet
-               :bind ("C-c y" . helm-yas-complete)))))
+            '(progn (use-package helm-c-yasnippet
+                      :bind ("C-c y" . helm-yas-complete))
 
+                    (defun my/yas-helm-prompt (prompt choices &optional display-fn)
+                      "Use helm to select a snippet. Put this into `yas/prompt-functions.'"
+                      (interactive)
+                      (setq display-fn (or display-fn 'identity))
+                      (if (require 'helm-config)
+                          (let (tmpsource cands result rmap)
+                            (setq cands (mapcar (lambda (x) (funcall display-fn x)) choices))
+                            (setq rmap (mapcar (lambda (x) (cons (funcall display-fn x) x)) choices))
+                            (setq tmpsource
+                                  (list
+                                   (cons 'name prompt)
+                                   (cons 'candidates cands)
+                                   '(action . (("Expand" . (lambda (selection) selection))))
+                                   ))
+                            (setq result (helm-other-buffer '(tmpsource) "*helm-select-yasnippet"))
+                            (if (null result)
+                                (signal 'quit "user quit!")
+                              (cdr (assoc result rmap))))
+                        nil))
+                    (setq yas-prompt-functions '(my/yas-helm-prompt yas-ido-prompt yas-no-prompt))))))
+
+
+;; snippet helper functions
+;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defun my/guess-user-name-from-git ()
+  "Return user's name from git-config."
   (s-trim (shell-command-to-string "git config --get user.name")))
 
 (defun my/guess-user-email-from-git ()
+  "Return user's email address from git-config."
   (s-trim (shell-command-to-string "git config --get user.email")))
 
 ;; (yas/setup "~/.emacs.d/plugin/yasnippet-0.6.1c")
