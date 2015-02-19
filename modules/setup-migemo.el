4c3654d278867b043a5aa7dbc53a21150e96dda6
Ryo Fukumuro <rkworks@o0o.co>
2015-02-19 18:21:33 +0900

    Make migemo optional

--- modules/setup-migemo.el
+++ modules/setup-migemo.el
@@ -6,12 +6,15 @@
 
 (use-package migemo
   :init (progn
-          (setq migemo-command "cmigemo")
-          (setq migemo-options '("-q" "--emacs"))
-          (setq migemo-dictionary "/usr/local/share/migemo/utf-8/migemo-dict")
-          (setq migemo-user-dictionary nil)
-          (setq migemo-regex-dictionary nil)
-          (setq migemo-coding-system 'utf-8-unix)
-          (migemo-init)))
+          (if (executable-find "cmigemo")
+              (progn
+                (setq migemo-command "cmigemo")
+                (setq migemo-options '("-q" "--emacs"))
+                (setq migemo-dictionary "/usr/local/share/migemo/utf-8/migemo-dict")
+                (setq migemo-user-dictionary nil)
+                (setq migemo-regex-dictionary nil)
+                (setq migemo-coding-system 'utf-8-unix)
+                (migemo-init))
+            (setq migemo-isearch-enable-p nil))))
 
 ;;; setup-migemo.el ends here
