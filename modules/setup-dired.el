709ca0bf8be2bcc247e3fb418d7c4e4ef3d4ec18
Ryo Fukumuro <rkworks@o0o.co>
2015-02-19 18:06:34 +0900

    Cleanup

--- modules/setup-dired.el
+++ modules/setup-dired.el
@@ -4,110 +4,44 @@
 
 ;;; Code:
 
-(defun my/dired-do-quicklook (files)
-  "preview with Quick Look."
-  (interactive
-   (list (setq files (if current-prefix-arg
-                         (list (dired-get-filename t t))
-                       (dired-get-marked-files t)))))
-  (let* ((current-line-file (dired-get-filename t t))
-         (former-list (member current-line-file files))
+(defun my/dired-do-quicklook ()
+  "Preview with Quick Look."
+  (interactive)
+  (let* ((files (if current-prefix-arg
+                    (list (dired-get-filename t t))
+                  (dired-get-marked-files t)))
+         (current-line-file (dired-get-filename t t))
+         (ht (-split-on current-line-file files))
+         (files (append (list current-line-file) (cadr ht) (car ht)))
          (process (get-process "qlmanage_ps")))
     (when process
       (kill-process process))
-    (when former-list
-      (dolist (ele former-list)
-        (delete ele files))
-      (setq files (append former-list files)))
     (apply 'start-process "qlmanage_ps" nil "qlmanage" "-px" files)))
 
-(defun my/dired-do-tar-cvzf (output-filename arg)
-  "マークしたファイルを tar cvzf でまとめて圧縮する."
-  (interactive "FOUTPUT FILENAME: \nP")
-  (or (when (string= "" (file-name-nondirectory output-filename))
-        ;; 出力ファイル名が入力されていなかったらエラーとする.
-        (message "ERROR: Output filename is empty."))
-      (when (file-exists-p
-             (setq output-filename
-                   (concat output-filename (unless (string-match "\\.t\\(ar\\.\\)?gz$" output-filename) ".tar.gz"))))
-        ;; 出力ファイル名と同じ名前のファイルが, 既にあった場合, 上書きするか問い合わせる.
-        (not (y-or-n-p (concat "file " output-filename " exists. overwrite the file ?"))))
-      ;; 実際に圧縮をする.
-      (let (add-file)
-        (when arg
-          (while (y-or-n-p "ADD FILE?")
-            (add-to-list 'add-file (file-relative-name (read-file-name "" nil nil t)))))
-        (dolist (ppp (dired-get-marked-files t))
-          (dired-do-shell-command (concat "tar cvzf " output-filename ppp ".tar.gz" " *") nil
-                                  (list ppp))))))
-
 (defun my/dired-do-zip (filelist output-filename)
-  ;; (interactive "FOUTPUT FILENAME: \nP")
-  "マークしたファイルをzipでまとめて圧縮する.前置引数を与えた場合は、各ファイルごとに圧縮ファイルを作成する."
+  "Compress marked files with zip. With prefix arg, create
+achived files for each files."
   (interactive
-   (list (setq filelist (dired-get-marked-files t))
+   (list (dired-get-marked-files t)
          (let ((default-output-file-name
-                 (concat default-directory (car filelist) ".zip")))
+                 (concat default-directory (car (dired-get-marked-files t)) ".zip")))
            (read-file-name (format "filename[%s]:" default-output-file-name) nil default-output-file-name))))
+  (unless (string-match "\\.zip$" output-filename)
+    (setq output-filename
+          (concat output-filename ".zip")))
   (save-window-excursion
     (message "processing... : %S" output-filename)
-    (or (when (string= "" (file-name-nondirectory output-filename))
+    (or (when (string-empty-p (file-name-nondirectory output-filename))
           (message "ERROR: Output filename is empty."))
-        (when (file-exists-p (setq output-filename
-                                   (concat output-filename (unless (string-match "\\.zip$" output-filename) ".zip"))))
+        (when (file-exists-p output-filename)
           (not (y-or-n-p (concat "file " output-filename " exists. overwrite the file ?"))))
         (cond ((and current-prefix-arg (> (length filelist) 1))
                (dolist (filename filelist)
                  (my/dired-do-zip (list filename) (concat default-directory filename ".zip"))))
               (t
-               (dired-do-shell-command (concat "zip -r " (shell-quote-argument output-filename) " *") nil filelist)
+               (dired-do-shell-command (concat "zip -r " (shell-quote-argument (expand-file-name output-filename)) " *") nil filelist)
                (revert-buffer)
                (message "COMPLETE"))))))
-
-(defun my/dired-do-unarchive-with-password (filelist password)
-  "マークの付いたパスワード付き書庫を解凍する."
-  (interactive
-   (list (setq filelist (dired-get-marked-files t))
-         (read-passwd "archive password: " nil)))
-
-  (message "%S" (list filelist password))
-  (save-window-excursion
-    (let (commandlist firstfile)
-      (dolist (filename filelist)
-        (message "processing...: %S" filename)
-        (let ((command nil))
-          (cond ((string-match "\\.zip$" filename)
-                 (setq command
-                       (concat
-                        "unzip "
-                        (when (and password (not (string= "" password)))
-                          (concat "-P " password " "))
-                        (if commandlist
-                            (shell-quote-argument filename)
-                          "*")
-                        (when current-prefix-arg
-                          (concat " -d " (file-name-sans-extension (shell-quote-argument filename)) "/"))
-                        (format " && growlnotify -a 'Emacs' -m 'done - %s' || growlnotify -a 'Emacs' -m 'failed - %s'" filename filename))))
-                ((string-match "^\\(?:.+\\.part0*1\\|[^.]+\\)\\.rar$" filename)
-                 (setq command
-                       (concat
-                        "unrar x "
-                        (when (and password (not (string= "" password)))
-                          (concat "-p" password " "))
-                        (if commandlist
-                            (shell-quote-argument filename)
-                          "*")
-                        (when current-prefix-arg
-                          (concat " " (file-name-sans-extension (shell-quote-argument filename)) "/"))
-                        (format " && growlnotify -a 'Emacs' -m 'done - %s' || growlnotify -a 'Emacs' -m 'failed - %s'" filename filename)))))
-          (message "filename: %S  command: %S" filename command)
-          (when command
-            (when (null commandlist) (setq firstfile filename))
-            (add-to-list 'commandlist command)
-            )))
-      (when commandlist
-        (message "%s" (mapconcat 'identity commandlist ";"))
-        (dired-do-async-shell-command (mapconcat 'identity commandlist ";") nil (list firstfile))))))
 
 (defun my/dired-mark-region (start end &optional arg)
   "Mark all files in region.  With prefix argument, unflag all
@@ -131,7 +65,6 @@ those files."
   (bind-keys :map dired-mode-map
              ([remap move-beginning-of-line] . my/dired-back-to-start-of-files)
              ("C-c C-z" . my/dired-do-zip)
-             ("C-c C-u" . my/dired-do-unarchive-with-password)
              ("* r" . my/dired-mark-region))
   (when mac?
     (bind-keys :map dired-mode-map
--- modules/setup-yasnippet.el
+++ modules/setup-yasnippet.el
@@ -49,46 +49,4 @@
   "Return user's email address from git-config."
   (s-trim (shell-command-to-string "git config --get user.email")))
 
-;; (yas/setup "~/.emacs.d/plugin/yasnippet-0.6.1c")
-
-;; (yas/initialize)
-
-
-;; Develop in ~/emacs.d/snippets, but also
-;; include snippets that come with yasnippet
-
-;; (mapc 'yas/load-directory yas/root-directory)
-;; (custom-set-variables '(yas-trigger-key "TAB"))
-
-;; ;; Include snippets for Buster.js
-;; (require 'buster-snippets)
-
-;; ;; Jump to end of snippet definition
-;; (define-key yas/keymap (kbd "C-<return>") 'yas/exit-all-snippets)
-
-;; ;; Inter-field navigation
-;; (defun yas/goto-end-of-active-field ()
-;;   (interactive)
-;;   (let* ((snippet (car (yas/snippets-at-point)))
-;;         (position (yas/field-end (yas/snippet-active-field snippet))))
-;;     (if (= (point) position)
-;;         (move-end-of-line-or-next-line)
-;;       (goto-char position))))
-
-;; (defun yas/goto-start-of-active-field ()
-;;   (interactive)
-;;   (let* ((snippet (car (yas/snippets-at-point)))
-;;         (position (yas/field-start (yas/snippet-active-field snippet))))
-;;     (if (= (point) position)
-;;         (move-start-of-line-or-prev-line)
-;;       (goto-char position))))
-
-;; (define-key yas/keymap (kbd "C-e") 'yas/goto-end-of-active-field)
-;; (define-key yas/keymap (kbd "C-a") 'yas/goto-start-of-active-field)
-
-;; No dropdowns please, yas
-;; (setq yas-prompt-functions '(yas/ido-prompt yas/completing-prompt))
-
-
-
 ;;; setup-yasnippet.el ends here
