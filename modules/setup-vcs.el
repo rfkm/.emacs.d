9208017572bf48291defac5de0b1d348153695f3
Ryo Fukumuro <rkworks@o0o.co>
2015-06-03 22:16:03 +0900

    Suppress magit warning

--- modules/setup-vcs.el
+++ modules/setup-vcs.el
@@ -8,8 +8,10 @@
   :bind (("C-x v d" . magit-status)
          ("C-x v L" . magit-key-mode-popup-logging))
   :init (progn
-          (setq magit-status-buffer-switch-function 'switch-to-buffer)
-          (setq magit-save-some-buffers nil)))
+          (setq magit-last-seen-setup-instructions "1.4.0"))
+  :config (progn
+            (setq magit-status-buffer-switch-function 'switch-to-buffer)
+            (setq magit-save-some-buffers nil)))
 
 (use-package git-messenger
   :bind ("C-x v p" . git-messenger:popup-message)
