374d07d5cf87299ab04e359a3a3135aecf0aff8f
Ryo Fukumuro <rfkm@nop.sh>
2016-03-01 19:25:58 +0900

    Fix broken projectile settings

--- modules/setup-projectile.el
+++ modules/setup-projectile.el
@@ -4,35 +4,36 @@
 
 ;;; Code:
 
+(eval-when-compile (require 'projectile))
+
 (use-package projectile
-  :bind (("C-c p p" . projectile-switch-project)
-         ("C-x g" . projectile-find-file)
-         ("C-x G" . my/projectile-helm-ag)
-         ("C-x B" . projectile-switch-to-buffer))
-  :init
-  (progn
-    (when mac?
-      (autoload #'my/projectile-sourcetree "projectile" nil t)
-      (bind-keys
-       ("C-c p G" . my/projectile-sourcetree))))
   :config
-  (progn
-    (setq projectile-completion-system 'helm)
-    (setq projectile-use-git-grep t)
-    (setq projectile-cache-file (locate-user-emacs-file ".projectile.cache"))
-    (setq projectile-known-projects-file (locate-user-emacs-file ".projectile-bookmarks.eld"))
-    (projectile-load-known-projects)
-    (projectile-global-mode 1)
+  (setq projectile-completion-system 'helm)
+  (setq projectile-use-git-grep t)
+  (setq projectile-cache-file (locate-user-emacs-file ".projectile.cache"))
+  (setq projectile-known-projects-file (locate-user-emacs-file ".projectile-bookmarks.eld"))
+  (setq projectile-switch-project-action 'projectile-dired)
+  (projectile-load-known-projects)
+  (projectile-global-mode 1)
 
-    (defun my/projectile-helm-ag ()
-      (interactive)
-      (helm-ag (projectile-project-root)))
+  (defun my/projectile-helm-ag ()
+    (interactive)
+    (helm-ag (projectile-project-root)))
 
-    (when mac?
-      (defun my/projectile-sourcetree ()
-        "Open the current project with SouceTree."
-        (interactive)
-        (projectile-with-default-dir (projectile-project-root)
-          (call-process "open" nil nil nil "." "-a" "SourceTree"))))))
+  (bind-keys :map projectile-mode-map
+             ("C-x G" . my/projectile-helm-ag)
+             ("C-x g" . projectile-find-file)
+             ("C-x B" . projectile-switch-to-buffer))
+
+  (bind-keys ("C-c p p" . projectile-switch-project))
+
+  (defun my/projectile-sourcetree ()
+    "Open the current project with SouceTree."
+    (interactive)
+    (projectile-with-default-dir (projectile-project-root)
+      (call-process "open" nil nil nil "." "-a" "SourceTree")))
+
+  (bind-keys :map projectile-mode-map
+             ("C-c p G" . my/projectile-sourcetree)))
 
 ;;; setup-projectile.el ends here
